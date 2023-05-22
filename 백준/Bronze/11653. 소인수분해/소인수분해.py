d179f0d63be62b58757d8e1e5431b0764b267745
Changhun Lee <leechanghun55@gmail.com>
2023-05-22 21:39:58 +0900

    [Bronze I] Title: 소인수분해, Time: 912 ms, Memory: 34216 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -12,21 +12,26 @@ def solution():
     quotient = input_value
     min_division_value = 2
 
+
     while quotient != 1:
         # 나눈 나머지 값이 0 인 경우
         # 해당 값으로 소인수 분해가 가능하다. -> 출력
 
         if quotient % min_division_value == 0 :
-            # print("quotient",quotient)
+
             print(min_division_value)
             # 나누셈 몫값 재설정
-            quotient = quotient // min_division_value
+            quotient = quotient / min_division_value
 
             # 최소 소인수 분해값 재설정
             min_division_value = 2
             continue
 
         min_division_value+=1
+
+
+
+
 
 if __name__ == '__main__':
     solution()
