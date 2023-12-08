f9c5091d9de13f94516a400a5550360ea5b33e41
Changhun Lee <leechanghun55@gmail.com>
2023-12-08 11:56:26 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,12 +1,3 @@
-import string
-
 def solution(age):
-    pivot_dict = {}
-    for idx,v in enumerate(string.ascii_lowercase):
-        pivot_dict[idx] = v
-
-        
-    answer = ""
-    for i in str(age):
-        answer += pivot_dict.get(int(i))
+    answer = ''
     return answer
