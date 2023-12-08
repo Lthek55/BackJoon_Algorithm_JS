ce88cdffbccee606a2a0ad0059bfc66ad9b4dff4
Changhun Lee <leechanghun55@gmail.com>
2023-12-08 12:04:23 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,3 +1,11 @@
 def solution(emergency):
+    sorted_emergency = sorted(emergency,reverse=True)
+    pivot_dict = {}
+        
+    for idx, v in enumerate(sorted_emergency,start=1):
+        pivot_dict[v] = idx
+
     answer = []
+    for j in emergency:
+        answer.append(pivot_dict[j])
     return answer
